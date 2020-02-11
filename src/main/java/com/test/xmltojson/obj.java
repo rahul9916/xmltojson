725361dbfb0e51acd8f1ac329cb50a955081b665
rahul <>
2020-02-11 14:34:14 +0530

    {/src/main/java/com/test/xmltojson/obj.java}

--- src/main/java/com/test/xmltojson/obj.java
+++ src/main/java/com/test/xmltojson/obj.java
@@ -6,12 +6,35 @@ package com.test.xmltojson;
 
 public class obj implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public obj() {
-    }
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "age")
+	private int age;
 
+	public obj() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public obj(java.lang.String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 
 }
